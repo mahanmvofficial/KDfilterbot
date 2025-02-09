a93b8b7466c022f73d9a2e5bc443e6db91eb5280
Mahan M V <73701328+mahanmvofficial@users.noreply.github.com>
2025-02-09 21:38:27 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -70,6 +70,7 @@ async def answer(bot, query):
             except Exception as e:
                 logger.exception(e)
                 f_caption=f_caption
+    if f_caption is None:
             f_caption = (
             f"{clean_file_name}\n\n"
             "✯ ━━━━━ ✧ ━━━━━━ ✯\n"
