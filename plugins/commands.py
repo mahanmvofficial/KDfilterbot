0af1ccfe492c45b7afdcd8b23051f2c69dead615
Mahan M V <73701328+mahanmvofficial@users.noreply.github.com>
2025-02-09 20:16:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 join_db = JoinReqs
 
-@Client.on_message(filters.command("start") & filters.incoming)
+@Client.on_message(filters.command("mahan") & filters.incoming)
 async def start(client, message):
     try:
         await message.react(emoji=random.choice(REACTIONS), big=True)
